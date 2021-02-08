d347b66aba3728b577f602e63795410aeb3d0d0c
soare23 <toma.mihai.sorin23@gmail.com>
2021-02-08 22:36:18 +0200

    added infinite scroll - WOP

--- src/App.js
+++ src/App.js
@@ -1,23 +1,76 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
+import Meme from './components/Meme';
+import InfiniteScroll from 'react-infinite-scroll-component';
+import axios from 'axios';
 
 function App() {
+  const [posts, setPosts] = useState([]);
+  const [isBottom, setIsBottom] = useState(false);
+
+  function getPosts() {
+    axios.get('http://localhost:4000/memes').then(({ data }) => {
+      console.log(data);
+      setPosts(data);
+    });
+  }
+
+  useEffect(() => {
+    getPosts();
+  }, []);
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:4000/memes', {
+        params: {
+          lastMemeId: ,
+        },
+      })
+      .then(({ data }) => {
+        console.log('new data');
+        console.log(data);
+        // setPosts([...posts], data);
+      });
+  }, [isBottom]);
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
+
+  function handleError(event, index) {
+    if (event.target.naturalHeight < 100 && event.target.naturalWidth < 200) {
+      let finalPosts = [...posts];
+      finalPosts.splice(index, 1);
+      setPosts(finalPosts);
+    }
+  }
+
+  function handleScroll() {
+    const scrollTop =
+      (document.documentElement && document.documentElement.scrollTop) ||
+      document.body.scrollTop;
+    const scrollHeight =
+      (document.documentElement && document.documentElement.scrollHeight) ||
+      document.body.scrollHeight;
+    if (scrollTop + window.innerHeight + 50 >= scrollHeight) {
+      console.log('bottom');
+      setIsBottom(true);
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {posts.map((post, index) => (
+        <div key={index} className="meme-container">
+          <Meme
+            title={post.title}
+            url={post.mediaUrl}
+            handleError={handleError}
+            index={index}
+          ></Meme>
+        </div>
+      ))}
     </div>
   );
 }
