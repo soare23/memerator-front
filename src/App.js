0665d685e73f0f8b3abf45717b9bd92f1cf49f6e
soare23 <toma.mihai.sorin23@gmail.com>
2021-02-09 00:06:09 +0200

    added infinite working infintite scrolling + loading - needs styling

--- src/App.js
+++ src/App.js
@@ -1,50 +1,41 @@
 import './App.css';
 import { useState, useEffect } from 'react';
 import Meme from './components/Meme';
-import InfiniteScroll from 'react-infinite-scroll-component';
 import axios from 'axios';
+import { makeStyles } from '@material-ui/core/styles';
+import CircularProgress from '@material-ui/core/CircularProgress';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+    '& > * + *': {
+      marginLeft: theme.spacing(2),
+    },
+  },
+}));
 
 function App() {
   const [posts, setPosts] = useState([]);
-  const [isBottom, setIsBottom] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+  const classes = useStyles();
 
   function getPosts() {
-    axios.get('http://localhost:4000/memes').then(({ data }) => {
+    axios.get('http://opsolutions.ro:4000/memes').then(({ data }) => {
       console.log(data);
-      setPosts(data);
+      setPosts((prevState) => [...prevState, ...data]);
+      setIsLoading(false);
     });
   }
 
   useEffect(() => {
+    if (!isLoading) return;
     getPosts();
-  }, []);
-
-  useEffect(() => {
-    axios
-      .get('http://localhost:4000/memes', {
-        params: {
-          lastMemeId: ,
-        },
-      })
-      .then(({ data }) => {
-        console.log('new data');
-        console.log(data);
-        // setPosts([...posts], data);
-      });
-  }, [isBottom]);
+  }, [isLoading]);
 
   useEffect(() => {
     window.addEventListener('scroll', handleScroll);
     return () => window.removeEventListener('scroll', handleScroll);
   }, []);
-
-  function handleError(event, index) {
-    if (event.target.naturalHeight < 100 && event.target.naturalWidth < 200) {
-      let finalPosts = [...posts];
-      finalPosts.splice(index, 1);
-      setPosts(finalPosts);
-    }
-  }
 
   function handleScroll() {
     const scrollTop =
@@ -54,23 +45,36 @@ function App() {
       (document.documentElement && document.documentElement.scrollHeight) ||
       document.body.scrollHeight;
     if (scrollTop + window.innerHeight + 50 >= scrollHeight) {
-      console.log('bottom');
-      setIsBottom(true);
+      setIsLoading(true);
+    }
+  }
+
+  function handleError(event, index) {
+    if (event.target.naturalHeight < 100 && event.target.naturalWidth < 200) {
+      let finalPosts = [...posts];
+      finalPosts.splice(index, 1);
+      setPosts(finalPosts);
     }
   }
 
   return (
     <div className="App">
-      {posts.map((post, index) => (
-        <div key={index} className="meme-container">
-          <Meme
-            title={post.title}
-            url={post.mediaUrl}
-            handleError={handleError}
-            index={index}
-          ></Meme>
+      {isLoading ? (
+        <div className={classes.root}>
+          <CircularProgress />
         </div>
-      ))}
+      ) : (
+        posts.map((post, index) => (
+          <div key={index} className="meme-container">
+            <Meme
+              title={post.title}
+              url={post.mediaUrl}
+              handleError={handleError}
+              index={index}
+            ></Meme>
+          </div>
+        ))
+      )}
     </div>
   );
 }
